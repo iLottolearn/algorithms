f35e495a6b732ab068837ee38a807b4dd1e05c34
L <minesteam@outlook.com>
2018-09-29 09:48:37 +0800

    快速排序

--- kuaisu.py
+++ kuaisu.py
@@ -5,5 +5,35 @@
 from random import randint
 import time
 
-l = [randint(1,10000)for i in range(10000)]
-def kuaisu(l):
+ary = [randint(1,10000)for i in range(10000)]
+
+def quick_sort(ary):
+    return qs(ary,0,len(ary)-1)
+
+def qs(ary,left,right):
+    if left >= right: return ary
+    key = ary[left]
+    lp = left
+    rp = right
+    while lp < rp:
+        while ary[rp] >= key and lp < rp:
+            rp -= 1
+        while ary[lp] <= key and lp < rp:
+            lp += 1
+        ary[lp],ary[rp] = ary[rp],ary[lp]
+    ary[lp],ary[left] = ary[left],ary[lp]
+    qs(ary,left,lp-1)
+    qs(ary,rp+1,right)
+    return ary
+
+def main():
+    start = time.clock()
+    reslut = quick_sort(ary)
+    print(reslut)
+    end = time.clock()
+    print("running time : %s"%(end -start))
+
+if __name__ == '__main__':
+    main()
+
+
